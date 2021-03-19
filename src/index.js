572b687ab9390f5b5f9662acac3251d98a719c5d
JZims <JZimmsGames@gmail.com>
2021-03-19 14:59:50 -0400

    Added preventDefault function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+let submitButton= document.querySelector("#create-task-form")
+
+
+submitButton.addEventListener("click", function(event){
+  event.preventDefault()
+})
+
