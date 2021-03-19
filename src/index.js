9d3ca78b3aa467d2f9a7f1652a2d8be730048042
matthewobrien <matthew.kevin.obrien@gmail.com>
2021-03-19 15:33:46 -0400

    Our initial attempt

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+let commentText = document.querySelector("Create New Task")
+let submitButton = document.querySelector("#create-task-form")
+let todos = document.querySelector("#tasks")
 
-let submitButton= document.querySelector("#create-task-form")
-
-
+commentText.addEventListener("submit",toDoList())
 submitButton.addEventListener("click", function(event){
   event.preventDefault()
 })
+
+
+function toDoList(){
+  var a = document.createElement("li")
+
+  todos.append(`${commentText}`, a)
+  console.log("Hello")
+}
 
